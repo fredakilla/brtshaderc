87f842f4f9d540a9bcd4c9fc5874ee835147e9a8
fredakilla <fredakilla@gmail.com>
2018-02-28 06:01:15 +0100

    fix indent

--- tools/brtshaderc/brtshaderc.cpp
+++ tools/brtshaderc/brtshaderc.cpp
@@ -62,29 +62,29 @@ namespace shaderc
 
 namespace bgfx
 {
-	typedef void(*UserErrorFn)(void*, const char*, va_list);
-	static UserErrorFn s_user_error_fn = nullptr;
-	static void* s_user_error_ptr = nullptr;
-	void setShaderCErrorFunction(UserErrorFn fn, void* user_ptr)
-	{
-		s_user_error_fn = fn;
-		s_user_error_ptr = user_ptr;
+    typedef void(*UserErrorFn)(void*, const char*, va_list);
+    static UserErrorFn s_user_error_fn = nullptr;
+    static void* s_user_error_ptr = nullptr;
+    void setShaderCErrorFunction(UserErrorFn fn, void* user_ptr)
+    {
+        s_user_error_fn = fn;
+        s_user_error_ptr = user_ptr;
     }
 }
 
 void printError(FILE* file, const char* format, ...)
 {
-	va_list args;
-	va_start(args, format);
-	if (bgfx::s_user_error_fn)
-	{
-		bgfx::s_user_error_fn(bgfx::s_user_error_ptr, format, args);
-	}
-	else
-	{
-		vfprintf(file, format, args);
-	}
-	va_end(args);
+    va_list args;
+    va_start(args, format);
+    if (bgfx::s_user_error_fn)
+    {
+        bgfx::s_user_error_fn(bgfx::s_user_error_ptr, format, args);
+    }
+    else
+    {
+        vfprintf(file, format, args);
+    }
+    va_end(args);
 }
 
 // hack to defined stuff
@@ -109,12 +109,12 @@ void printError(FILE* file, const char* format, ...)
 namespace bgfx 
 {
     bool compilePSSLShader(const Options&, uint32_t, const std::string&, bx::WriterI*)
-	{
-		return false;		
-	}
-	bool compileSPIRVShader(const Options&, uint32_t, const std::string&, bx::WriterI*)
-	{
-		return false;
+    {
+        return false;
+    }
+    bool compileSPIRVShader(const Options&, uint32_t, const std::string&, bx::WriterI*)
+    {
+        return false;
     }
 }
 
